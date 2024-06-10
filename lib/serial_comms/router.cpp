c5240b9caf45da1dca727633162c9bcc6fd94997
Ishan Goel <igoel.mail@gmail.com>
2024-06-09 21:18:34 -0700

    initial version of router

--- lib/serial_comms/router.cpp
+++ lib/serial_comms/router.cpp
@@ -3,3 +3,36 @@
 //
 
 #include "router.h"
+
+vector<func> router::funcs;
+
+void router::send(char msg[], int len) {
+    COMMS_SERIAL.write(msg, len);
+}
+
+void router::receive(char msg[], int len) {
+    COMMS_SERIAL.readBytes(msg, len);
+}
+
+void router::add(func f) {
+    if (funcs.empty()) {
+        init(); // nice way to avoid having to call init() in main
+    }
+    funcs.push_back(f);
+}
+
+void router::init() {
+    COMMS_SERIAL.begin(COMMS_RATE);
+    COMMS_SERIAL.setTimeout((unsigned long) -1); // wrap around to max long so we never time out
+}
+
+[[noreturn]] void router::run() { // attribute here enables dead-code warning & compiler optimization
+    while (true) {
+        String s = COMMS_SERIAL.readStringUntil(0, 200); // read until null terminator or 200 characters
+        for (auto &f : funcs) {                                   // (one would hope none of our functions have names that long lol)
+            if (s.equals(f.name)) {
+                f.f(); // call the function. it can decide to send, receive or whatever.
+            }
+        }
+    }
+}
--- lib/serial_comms/router.h
+++ lib/serial_comms/router.h
@@ -5,10 +5,40 @@
 #ifndef TADPOLE_SOFTWARE_ROUTER_H
 #define TADPOLE_SOFTWARE_ROUTER_H
 
+#include <vector>
+#include <Wire.h>
+
+using namespace std;
+
+#define COMMS_SERIAL Serial
+#define COMMS_RATE 9600
+
+struct func;
 
 class router {
-
+private:
+    // funcs is a vector of function pointers and their names
+    static vector<func> funcs;
+    router(); // prevent instantiation
+    static void init();
+public:
+    // send sends a message over the serial port. the caller is responsible for
+    // freeing the memory of the message
+    static void send(char msg[], int len);
+    // receive reads a message from the serial port into the supplied buffer.
+    // the caller is responsible for freeing the memory of the message
+    static void receive(char msg[], int len);
+    // add registers a new function to the router
+    static void add(func f);
+    // run starts monitoring the serial port for messages and calls the
+    // appropriate function when a message is received. this function never
+    // returns.
+    [[noreturn]] static void run();
 };
 
+struct func {
+    void (*f)();
+    char *name;
+};
 
 #endif //TADPOLE_SOFTWARE_ROUTER_H
