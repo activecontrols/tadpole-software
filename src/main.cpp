e341bf0822d3bb961dd462095f96b57ec518c009
vpalmerio <vpalmerio@protonmail.com>
2024-06-11 19:20:42 +0100

    Disable ODrive setup in main if desired

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "../lib/serial_comms/Router.h"
 #include "../lib/data/Loader.h"
 
+#define ENABLE_ODRIVE_COMM false
+
 void ping() {
     char msg[] = "pong";
     Router::send(msg, sizeof(msg));
@@ -11,7 +13,9 @@ void ping() {
 void setup() {
     Router::add({ping, "ping"}); // example registration
     Loader::begin(); // registers loader functions with the router
+#if (ENABLE_ODRIVE_COMM)
     ODriveController::setupODrives(); // sets up the LOX and Fuel odrives
+#endif
     Router::run(); // nothing runs after this. router handles everything
 }
 
