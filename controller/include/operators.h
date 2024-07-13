195a6ba48baa3ba7df70b11ac87f7e3a1994fee7
vpalmerio <vpalmerio@protonmail.com>
2024-07-13 15:34:41 -0500

    Added operators.h for easier string manipulation across entire project

---
+++ controller/include/operators.h
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+//Add strings together using stream `<<` operator
+String operator<<(String lhs, String rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+//Add float to a string together using stream `<<` operator
+String operator<<(String &lhs, float rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+//Add int to a string together using stream `<<` operator
+String operator<<(String &lhs, int rhs) {
+    lhs += rhs;
+    return lhs;
+}
+
+//Print with stream `<<` operator, taken from the ODriveUART.cpp file
+template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
+template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
