afeb63f893c55b5197138c593831102ae6514fef
RobertJN64 <robertjnies@gmail.com>
2024-10-17 21:26:22 -0400

    done - just need to check units

--- controller/lib/valve_controller/valve_controller.cpp
+++ controller/lib/valve_controller/valve_controller.cpp
@@ -1,26 +1,47 @@
 #include <valve_controller.hpp>
 
-#define INTERPOLATION_TABLE_LENGTH 20
+// TODO - efficiency coeff?
+#define tadpole_AREA_OF_THROAT 1.69      // in^2
+#define tadpole_C_STAR 4998.0654         // ft / s
+#define tadpole_MASS_FLOW_RATIO 1.2      // #ox = 1.2 * fuel
+#define GRAVITY_FT_S 32.1740             // Gravity in ft / s^2
+#define GRAVITY_IN_S (GRAVITY_FT_S * 12) // Gravity in in / s^2
 
+#define cav_vent_ox_AREA_OF_THROAT 0.00989 // in^2
+#define cav_vent_ox_CD 0.975               // unitless
+#define fluid_ox_SPECIFIC_GRAVITY 1.15     // unitless
+#define fluid_ox_VAPOUR_PRESSURE 14.2      // psi
+#define fluid_ox_DENSITY 0.04146           // lb/in^3
+#define fluid_ox_UPSTREAM_PRESSURE 820     // psi
+
+#define cav_vent_ipa_AREA_OF_THROAT 0.00989 // in^2
+#define cav_vent_ipa_CD 0.975               // unitless
+#define fluid_ipa_SPECIFIC_GRAVITY 0.788    // unitless
+#define fluid_ipa_VAPOUR_PRESSURE 0.638     // psi
+#define fluid_ipa_DENSITY 0.02836           // lb/in^3
+#define fluid_ipa_UPSTREAM_PRESSURE 820     // psi
+
+#define INTERPOLATION_TABLE_LENGTH 20 // max length of all tables - set to enable passing tables to functions
+#define VALVE_ANGLE_TABLE_LEN 11
 float valve_angle_table[2][INTERPOLATION_TABLE_LENGTH] = {
     {0.000, 0.070, 0.161, 0.378, 0.670, 1.000, 1.450, 2.050, 2.780, 3.710, 4.960},
     {0, 9, 18, 27, 36, 45, 54, 63, 72, 81, 90}};
+#define CF_THRUST_TABLE_LEN 2
+float cf_thrust_table[2][INTERPOLATION_TABLE_LENGTH] = {
+    {220, 550},
+    {1.12, 1.3}};
 
-// TODO - efficiency coeff?
-#define tadpole_AREA_OF_THROAT 1.69 // in^2
-#define tadpole_C_STAR 4998.0654    // ft / s
-#define tadpole_C_F 1.12            // unitless - TODO - why does this vary with thrust?
-#define tadpole_MASS_FLOW_RATIO 1.2 // #ox = 1.2 * fuel
-#define GRAVITY_FT_S 32.1740        // Gravity in ft / s^2
-#define LIQUID_SPECIFIC_GRAVITY 1.5 // TODO - set this
-#define UPSTREAM_PRESSURE 1000      // psi       // TODO - unit conversions // needs to be user configurable
+struct fluid {
+  float cav_vent_throat_area;
+  float cav_vent_cd;
+  float specific_gravity;
+  float vapour_pressure;
+  float density;
+  float upstream_pressure;
+};
 
-// CD * area of throat for cav venturi
-
-// liquid specific
-// LIQUID_SPECIFIC_GRAVITY
-// vapour pressure
-// density
+fluid ox{cav_vent_ox_AREA_OF_THROAT, cav_vent_ox_CD, fluid_ox_SPECIFIC_GRAVITY, fluid_ox_VAPOUR_PRESSURE, fluid_ox_DENSITY, fluid_ox_UPSTREAM_PRESSURE};
+fluid ipa{cav_vent_ipa_AREA_OF_THROAT, cav_vent_ipa_CD, fluid_ipa_SPECIFIC_GRAVITY, fluid_ipa_VAPOUR_PRESSURE, fluid_ipa_DENSITY, fluid_ox_UPSTREAM_PRESSURE};
 
 // maps v from (min_in, max_in) to (min_out, max_out)
 float linear_interpolation(float v, float min_in, float max_in, float min_out, float max_out) {
@@ -39,24 +60,25 @@ float clamped_table_interplolation(float v, float table[2][INTERPOLATION_TABLE_L
   }
   return table[1][table_length - 1]; // if starting value is above max, return max
 }
-
-// convert chamber pressure to total mass flow rate
-// https://en.wikipedia.org/wiki/Characteristic_velocity
-// OUTPUT: total mass flow rate
-float mass_flow_rate(float chamber_pressure) {
-  return chamber_pressure * tadpole_AREA_OF_THROAT / tadpole_C_STAR * GRAVITY_FT_S;
-}
+#pragma endregion
 
 // find CF in terms of thrust using linear interpolation
 float cf(float thrust) {
-  return (thrust - 40) / 60 * (1.3 - 1.12) + 1.12;
+  return clamped_table_interplolation(thrust, cf_thrust_table, CF_THRUST_TABLE_LEN);
 }
 
 // convert thrust to chamber pressure
 // https://seitzman.gatech.edu/classes/ae4451/thrust_coefficient.pdf
 // OUTPUT: chamber pressure
 float chamber_pressure(float thrust) {
-  return thrust / tadpole_C_F / tadpole_AREA_OF_THROAT;
+  return thrust / cf(thrust) / tadpole_AREA_OF_THROAT;
+}
+
+// convert chamber pressure to total mass flow rate
+// https://en.wikipedia.org/wiki/Characteristic_velocity
+// OUTPUT: total mass flow rate
+float mass_flow_rate(float chamber_pressure) {
+  return chamber_pressure * tadpole_AREA_OF_THROAT / tadpole_C_STAR * GRAVITY_FT_S;
 }
 
 // convert total mass flow into OX and FUEL flow rates
@@ -66,23 +88,29 @@ void mass_balance(float total_mass_flow, float *mass_flow_ox, float *mass_flow_f
   *mass_flow_fuel = total_mass_flow / (1 + tadpole_MASS_FLOW_RATIO);
 }
 
-// convert cv into valve angle
-// OUTPUT: valve angle (radians)
-float valve_angle(float cv) {
-  return clamped_table_interplolation(cv, valve_angle_table, 11);
-} // TODO - convert this to radians
+// convert mass_flow into pressure
+// OUTPUT: pressure (psi)
+float cavitating_venturi(float mass_flow, fluid cvFluid) {
+  return pow(mass_flow / cvFluid.cav_vent_throat_area / cvFluid.cav_vent_cd, 2) / 2 / GRAVITY_IN_S / cvFluid.density + cvFluid.vapour_pressure;
+}
 
 // convert mass_flow into valve cv
 // OUTPUT: cv (unitless)
-float sub_critical_cv(float mass_flow, float downstream_pressure) {
-  return 1.17 * mass_flow / sqrt(UPSTREAM_PRESSURE - downstream_pressure * LIQUID_SPECIFIC_GRAVITY);
+float sub_critical_cv(float mass_flow, float downstream_pressure, fluid cvFluid) {
+  return mass_flow / sqrt((cvFluid.upstream_pressure - downstream_pressure) * cvFluid.specific_gravity);
 }
+
+// convert cv into valve angle
+// OUTPUT: valve angle (radians)
+float valve_angle(float cv) {
+  return clamped_table_interplolation(cv, valve_angle_table, VALVE_ANGLE_TABLE_LEN);
+} // TODO - convert this to radians
 
 // get valve angles given thrust
 void open_loop_thrust_control(float thrust, float *angle_ox, float *angle_fuel) {
   float mass_flow_ox;
   float mass_flow_fuel;
   mass_balance(mass_flow_rate(chamber_pressure(thrust)), &mass_flow_ox, &mass_flow_fuel);
-  *angle_ox = valve_angle(sub_critical_cv(mass_flow_ox, 0));
-  *angle_fuel = valve_angle(sub_critical_cv(mass_flow_fuel, 0));
+  *angle_ox = valve_angle(sub_critical_cv(mass_flow_ox, cavitating_venturi(mass_flow_ox, ox), ox));
+  *angle_fuel = valve_angle(sub_critical_cv(mass_flow_fuel, cavitating_venturi(mass_flow_fuel, ipa), ipa));
 }
