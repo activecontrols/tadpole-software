bab187b9a7b16d7582b3eafc039f83972a11d68a
RobertJN64 <robertjnies@gmail.com>
2024-11-09 15:13:09 -0500

    curve writer improved

--- curve_writer/curve_writer.cpp
+++ curve_writer/curve_writer.cpp
@@ -2,34 +2,47 @@
 // (PLEASE DON'T RUN ON THE TEENSY)
 
 #include "../Curve.h"
-#include <fstream>
 #include <assert.h>
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <cstring>
 
-int main() {
-  curve_header header;
-  header.ctype = curve_type::lerp;
-  header.is_open = false;
-  header.of_ratio = 1.2;
-  header.lerp.checksum[0] = 'H';
-  header.lerp.checksum[1] = 'E';
-  header.lerp.checksum[2] = 'L';
-  header.lerp.checksum[3] = 'P';
+#define MODE_THRUST 't'
+#define MODE_ANGLES 'a'
 
-  std::string csv_filename;
-  std::cout << "Enter the CSV filename: ";
-  std::getline(std::cin, csv_filename);
+curve_header header;           // file data to write
+lerp_point_thrust *ltc = NULL; // lerp thrust curve
+lerp_point_angle *lac = NULL;  // lerp angle curve
 
-  int rows = 0;
+char input_mode() {
+  char letter;
+  do {
+    std::cout << "Enter Mode (Thrust / Angle) [t/a]: ";
+    std::cin >> letter;
+    if (letter != MODE_ANGLES && letter != MODE_THRUST) {
+      std::cout << "\nMode must be 'a' or 't'" << std::endl;
+    }
+    std::cin.ignore(); // ignore newline left in buffer
+  } while (letter != MODE_ANGLES && letter != MODE_THRUST);
+  return letter;
+}
 
-  std::ifstream csv_file(csv_filename);
-  if (!csv_file.is_open()) {
-    std::cerr << "Error opening file: " << csv_filename << std::endl;
-    return 0;
+// reads a csv cell into target
+void read_csv_cell(std::stringstream *ss, float *target) {
+  std::string cell = ""; // holds one CSV value
+  std::getline(*ss, cell, ',');
+  if (cell == "") {
+    throw std::runtime_error("Malformed CSV - missing column");
   }
-  std::string line;
+  *target = std::stof(cell);
+}
+
+// returns number of rows read
+int read_csv(std::ifstream &csv_file, char mode) {
+  int rows = 0;     // number of rows of data
+  std::string line; // holds each line
+
   while (std::getline(csv_file, line)) {
     rows += 1;
   }
@@ -37,20 +50,33 @@ int main() {
   csv_file.clear();
   csv_file.seekg(0);
 
-  lerp_point_closed *lcs = (lerp_point_closed *)malloc(rows * sizeof(lerp_point_closed));
-
-  for (int i = 0; i < rows; i++) {
-    std::getline(csv_file, line);
-    std::stringstream ss(line);
-    std::string cell;
-    std::getline(ss, cell, ',');
-    lcs[i].time = std::stof(cell);
-    std::getline(ss, cell, ',');
-    lcs[i].thrust = std::stof(cell);
+  if (mode == MODE_THRUST) {
+    std::cout << "Reading file - expecting columns time (s), thrust (lbf) - don't include header row" << std::endl;
+    ltc = (lerp_point_thrust *)malloc(rows * sizeof(lerp_point_thrust));
+    for (int i = 0; i < rows; i++) {
+      std::getline(csv_file, line);
+      std::stringstream ss(line);
+      read_csv_cell(&ss, &ltc[i].time);
+      read_csv_cell(&ss, &ltc[i].thrust);
+    }
+  } else {
+    std::cout << "Reading file - expecting columns time (s), lox_angle (deg), ipa_angle (deg) - don't include header row" << std::endl;
+    lac = (lerp_point_angle *)malloc(rows * sizeof(lerp_point_angle));
+    for (int i = 0; i < rows; i++) {
+      std::getline(csv_file, line);
+      std::stringstream ss(line);
+      read_csv_cell(&ss, &lac[i].time);
+      read_csv_cell(&ss, &lac[i].lox_angle);
+      read_csv_cell(&ss, &lac[i].ipa_angle);
+    }
   }
-
   csv_file.close();
 
+  return rows;
+}
+
+void fill_header(std::string csv_filename, char mode, int num_points) {
+  csv_filename = "autogen from " + csv_filename;
   for (size_t i = 0; i < sizeof(header.curve_label); i++) {
     if (i > csv_filename.length()) {
       header.curve_label[i] = '\0';
@@ -59,17 +85,47 @@ int main() {
     }
   }
 
-  std::strcpy(header.curve_label, csv_filename.c_str());
-  header.lerp.num_points = rows;
+  header.ctype = curve_type::lerp; // only support lerp curves
+  header.of_ratio = 1.2;           // this is only needed for sine / chirp curves
+  header.is_thrust = mode == MODE_THRUST;
+  header.lerp.num_points = num_points;
+}
 
+void write_file(char mode) {
   std::ofstream file;
   file.open("out.hex", std::ios_base::binary);
   assert(file.is_open());
   file.write((char *)&header, sizeof(header));
-  file.write((char *)lcs, sizeof(lerp_point_closed) * header.lerp.num_points);
-
+  if (mode == MODE_THRUST) {
+    file.write((char *)ltc, sizeof(lerp_point_thrust) * header.lerp.num_points);
+  } else {
+    file.write((char *)lac, sizeof(lerp_point_angle) * header.lerp.num_points);
+  }
   file.close();
+}
 
+int main() {
+  char mode;                // thrust or angle
+  int num_pts;              // number of points
+  std::string csv_filename; // holds filename
+
+  // USER INPUT
+  std::cout << "Curve Writer" << std::endl;
+  std::cout << "Running on curve version: " << header.version << std::endl;
+  mode = input_mode();
+  std::cout << "Enter the CSV filename: ";
+  std::getline(std::cin, csv_filename);
+
+  std::ifstream csv_file(csv_filename);
+  if (!csv_file.is_open()) {
+    std::cerr << "Error opening file: " << csv_filename << std::endl;
+    return 0;
+  }
+
+  num_pts = read_csv(csv_file, mode);
+  fill_header(csv_filename, mode, num_pts);
+
+  write_file(mode);
   std::cout << "File output to out.hex" << std::endl;
 
   return 0;
