8339792ffd5b13915794f6cbb99d6a5dd9ffea42
RobertJN64 <robertjnies@gmail.com>
2025-02-01 16:56:08 -0500

    fix typo in demux

--- controller/lib/spi_demux/spi_demux.cpp
+++ controller/lib/spi_demux/spi_demux.cpp
@@ -9,7 +9,7 @@ void SPI_Demux::begin() {
 }
 
 void SPI_Demux::select_chip(int chip_id) {
-  digitalWrite(SPI_DEMUX_BIT_0, chip_id & 0b0000);
+  digitalWrite(SPI_DEMUX_BIT_0, chip_id & 0b0001);
   digitalWrite(SPI_DEMUX_BIT_1, chip_id & 0b0010);
   digitalWrite(SPI_DEMUX_BIT_2, chip_id & 0b0100);
   digitalWrite(SPI_DEMUX_BIT_3, chip_id & 0b1000);
