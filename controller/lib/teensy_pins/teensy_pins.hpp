66f25ac8b8ae05479b1e8d164b4e60527f0eb21d
RobertJN64 <robertjnies@gmail.com>
2025-03-08 20:08:19 -0500

    teensy pins setup for new box wiring

--- controller/lib/teensy_pins/teensy_pins.hpp
+++ controller/lib/teensy_pins/teensy_pins.hpp
@@ -12,10 +12,15 @@
 
 // Demux System
 
-#define SPI_DEMUX_BIT_0 37 // marked as CS_BIN_4
-#define SPI_DEMUX_BIT_1 36 // marked as CS_BIN_0
-#define SPI_DEMUX_BIT_2 35 // marked as CS_BIN_1
-#define SPI_DEMUX_BIT_3 34 // marked as CS_BIN_2
-#define SPI_DEMUX_BIT_4 33 // marked as CS_BIN_3
+#define SPI_DEMUX_BIT_0 29 // marked as CS_BIN_4
+#define SPI_DEMUX_BIT_1 28 // marked as CS_BIN_0
+#define SPI_DEMUX_BIT_2 4  // marked as CS_BIN_1
+#define SPI_DEMUX_BIT_3 3  // marked as CS_BIN_2
+#define SPI_DEMUX_BIT_4 2  // marked as CS_BIN_3
+
+// Other
+// SPI MISO 1
+// SPI MOSI 26
+// SPI SCK 27
 
 #endif
