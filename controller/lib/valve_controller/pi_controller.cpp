fca1ddfee3e92aaf9a7e50b7e60aa59809c23edd
RobertJN64 <robertjnies@gmail.com>
2025-04-03 15:43:41 -0400

    implement PID controller clamping + reset

--- controller/lib/valve_controller/pi_controller.cpp
+++ controller/lib/valve_controller/pi_controller.cpp
@@ -1,9 +1,10 @@
 #include "pi_controller.hpp"
 #include <Arduino.h>
 
-PI_Controller::PI_Controller(float kp, float ki) {
+PI_Controller::PI_Controller(float kp, float ki, float max_output) {
   this->kp = kp;
   this->ki = ki;
+  this->max_output = max_output;
 }
 
 float PI_Controller::compute(float input_error) {
@@ -14,11 +15,30 @@ float PI_Controller::compute(float input_error) {
 
   this->err_sum += input_error * (this_compute_time - last_compute_time) * 0.001;
   this->last_compute_time = this_compute_time;
-  return this->kp * input_error + this->ki * this->err_sum;
+
+  float raw_output = this->kp * input_error + this->ki * this->err_sum;
+  if (raw_output > max_output) {
+    return max_output; // clamped high
+  }
+  if (raw_output < -max_output) {
+    return -max_output; // clamped low
+  }
+  return raw_output;
+}
+
+float PI_Controller::reset() {
+  long long last_compute_time = -1;
+  float err_sum = 0;
 }
 
 namespace ClosedLoopControllers {
-PI_Controller Chamber_Pressure_Controller(1, 1); // TODO RJN CL - PID constants
-PI_Controller LOX_Angle_Controller(1, 1);
-PI_Controller IPA_Angle_Controller(1, 1);
+PI_Controller Chamber_Pressure_Controller(1, 1, INFINITY); // TODO RJN CL - PID constants
+PI_Controller LOX_Angle_Controller(1, 1, INFINITY);
+PI_Controller IPA_Angle_Controller(1, 1, INFINITY);
+
+void reset() {
+  Chamber_Pressure_Controller.reset();
+  LOX_Angle_Controller.reset();
+  IPA_Angle_Controller.reset();
+}
 } // namespace ClosedLoopControllers
--- controller/lib/valve_controller/pi_controller.hpp
+++ controller/lib/valve_controller/pi_controller.hpp
@@ -3,18 +3,22 @@
 
 class PI_Controller {
 public:
-  PI_Controller(float kp, float ki);
+  PI_Controller(float kp, float ki, float max_output);
+  float reset();
   float compute(float input_error);
 
 private:
   float kp;
   float ki;
+  float max_output;
 
   long long last_compute_time = -1;
   float err_sum = 0;
 };
 
 namespace ClosedLoopControllers {
+void reset();
+
 extern PI_Controller Chamber_Pressure_Controller;
 extern PI_Controller LOX_Angle_Controller;
 extern PI_Controller IPA_Angle_Controller;
