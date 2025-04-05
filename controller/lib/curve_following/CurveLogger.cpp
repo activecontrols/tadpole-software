6b5b9d4cebe50c99d9e7c39efb6eb1e2cfaf8865
RobertJN64 <robertjnies@gmail.com>
2025-04-05 14:28:27 -0400

    log water angle controller and ff correctly

--- controller/lib/curve_following/CurveLogger.cpp
+++ controller/lib/curve_following/CurveLogger.cpp
@@ -11,10 +11,11 @@ namespace CurveLogger {
 File odriveLogFile;
 CString<400> curveTelemCSV;
 
-#define LOG_HEADER ("time,phase,thrust_cmd,lox_pos_cmd,ipa_pos_cmd,"           \
-                    "lox_pos,lox_vel,lox_voltage,lox_current,lox_temperature," \
-                    "ipa_pos,ipa_vel,ipa_voltage,ipa_current,ipa_temperature," \
-                    "water_tank_pressure,water_venturi_upstream_pressure,water_venturi_throat_pressure")
+#define LOG_HEADER ("time,phase,thrust_cmd,lox_pos_cmd,ipa_pos_cmd,"                                     \
+                    "lox_pos,lox_vel,lox_voltage,lox_current,lox_temperature,"                           \
+                    "ipa_pos,ipa_vel,ipa_voltage,ipa_current,ipa_temperature,"                           \
+                    "water_tank_pressure,water_venturi_upstream_pressure,water_venturi_throat_pressure," \
+                    "water_angle_controller_p_component,water_angle_controller_i_component,ol_water_angle")
 
 // logs time, phase, thrust, and sensor data in .csv format
 void log_curve_csv(float time, int phase, float thrust, Sensor_Data sd) {
@@ -22,7 +23,8 @@ void log_curve_csv(float time, int phase, float thrust, Sensor_Data sd) {
   curveTelemCSV.clear();
   curveTelemCSV << time << "," << phase << "," << thrust << "," << Driver::loxODrive.getLastPosCmd() << "," << Driver::ipaODrive.getLastPosCmd() << ","
                 << Driver::loxODrive.getTelemetryCSV() << "," << Driver::ipaODrive.getTelemetryCSV() << ","
-                << sd.water.tank_pressure << "," << sd.water.venturi_upstream_pressure << "," << sd.water.venturi_throat_pressure;
+                << sd.water.tank_pressure << "," << sd.water.venturi_upstream_pressure << "," << sd.water.venturi_throat_pressure
+                << cs.water_angle_controller_p_component << "," << cs.water_angle_controller_i_component << "," << ff_state.ol_water_angle;
 
   curveTelemCSV.print();
   odriveLogFile.println(curveTelemCSV.str);
