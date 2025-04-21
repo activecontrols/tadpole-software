6ba101cf8caeaed7b6b49c4f5dd0c986e32a524a
RobertJN64 <robertjnies@gmail.com>
2025-04-21 01:53:04 -0400

    better command to pull file

--- curve_writer/pull_file.py
+++ curve_writer/pull_file.py
@@ -6,17 +6,16 @@ port = 'COM10'
 fname = input("Enter a filename and press enter to activate the serial dump: ")
 device = serial.Serial(port, 115200, timeout=1)
 
-device.write("cat\n".encode())
+device.write("auto_cat\n".encode())
 sleep(1)
 device.readline() #ignore the prompt
 device.write(f"{fname}\n".encode())
 
 with open("curve_writer/data/" + fname, "w+") as f:
     while True:
-        lines = device.readlines()
-        if (len(lines) == 0):
+        line = device.readline().decode()
+        if (line.strip() == 'done'):
             break
-        for line in lines:
-            line = line.decode()
-			# print(line.strip())
-            f.write(line.strip() + "\n")
+        print(line.strip())
+        f.write(line.strip() + "\n")
+        device.write("\n".encode())
