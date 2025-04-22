88611282c748b5525bd46038d1ee72b363a9b344
RobertJN64 <robertjnies@gmail.com>
2025-04-22 01:35:42 -0400

    log chamber pressure properly

--- controller/lib/curve_following/CurveLogger.cpp
+++ controller/lib/curve_following/CurveLogger.cpp
@@ -14,6 +14,7 @@ CString<400> curveTelemCSV;
 #define LOG_HEADER ("time,phase,thrust_cmd,lox_pos_cmd,ipa_pos_cmd,"                                   \
                     "lox_pos,lox_vel,lox_voltage,lox_current,lox_temperature,"                         \
                     "ipa_pos,ipa_vel,ipa_voltage,ipa_current,ipa_temperature,"                         \
+                    "chamber_pressure,"                                                                \
                     "lox_tank_pressure,lox_venturi_upstream_pressure,lox_venturi_throat_pressure,"     \
                     "lox_venturi_temperature,lox_valve_temperature,"                                   \
                     "ipa_tank_pressure,ipa_venturi_upstream_pressure,ipa_venturi_throat_pressure,"     \
@@ -28,6 +29,7 @@ void log_curve_csv(float time, int phase, float thrust, Sensor_Data sd) {
   curveTelemCSV.clear();
   curveTelemCSV << time << "," << phase << "," << thrust << "," << Driver::loxODrive.getLastPosCmd() << "," << Driver::ipaODrive.getLastPosCmd() << ","
                 << Driver::loxODrive.getTelemetryCSV() << "," << Driver::ipaODrive.getTelemetryCSV() << ","
+                << sd.chamber_pressure << ","
                 << sd.ox.tank_pressure << "," << sd.ox.venturi_upstream_pressure << "," << sd.ox.venturi_throat_pressure << ","
                 << sd.ox.venturi_temperature << "," << sd.ox.valve_temperature << ","
                 << sd.ipa.tank_pressure << "," << sd.ipa.venturi_upstream_pressure << "," << sd.ipa.venturi_throat_pressure << ","
